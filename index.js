9ad5ebf1063740968c5499af9a7730673bb078f0
Parth Patel <parth8733@gmail.com>
2022-09-30 12:54:17 -0400

    Updated Port

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ mongoose.connect(process.env.DATABASE_URL, { useNewURLParser: true }, () => {
   console.log("Database Connection established");
 });
 app.use(express.json());
-app.use('/Pokemon', pokemonRoute);
 app.get("/",(req, res) => {
     res.send("Welecome to Pokemon World!");
 })
+app.use('/Pokemon', pokemonRoute);
 
-app.listen(8800, () => {
-  console.log("Backend server is running");
-});
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Always watching... on port ${port}`));
+
