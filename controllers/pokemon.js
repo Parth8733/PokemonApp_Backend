f76030a72e0bc1f1b960876dcd503fca64a49cb3
Parth Patel <parth8733@gmail.com>
2022-09-30 12:33:23 -0400

    Create insert endpoint

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,4 +1,20 @@
+const Pokemon = require('../models/Pokemon');
 
 exports.show = (req, res) => {
     res.send("Welecome to Pokemon Center!");
 } 
+
+exports.create = async (req, res)=> {
+    const newPokemon = new Pokemon({
+        name:req.body.name,
+        types:req.body.types,
+        image_url:req.body.image_url
+    })
+    try{
+       const pokemon = await newPokemon.save();
+        res.status(200).json(pokemon);
+    }catch(err){
+        res.status(400).json(err.message);
+        console.log(err);
+    }
+}
